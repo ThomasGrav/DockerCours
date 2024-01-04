96f1b69d4a7adf9090fff2f6ba276ecb863ade53
ThomasGrav <94170746+ThomasGrav@users.noreply.github.com>
2024-01-04 13:56:34 +0100

    Update bcancer.py

--- bcancer.py
+++ bcancer.py
@@ -2,7 +2,6 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-import pandas as pd
 
 # Charger le jeu de données sur le cancer du sein
 data = load_breast_cancer()
